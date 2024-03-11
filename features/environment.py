4880fb19aec85a46538d990f7fc6b3d9cdbade75
Sandhu <aryan.sandhu@shell.com>
2024-03-12 01:08:32 +0530

    resolved some issues

--- features/environment.py
+++ features/environment.py
@@ -1,6 +1,4 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
 import allure
 from allure_commons.types import AttachmentType
 
